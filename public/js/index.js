5582a4ac5527bef3e95748097def5cd6fc237756
SVM143 <SOURAV.VIKAS6@GMAIL.COM>
2017-09-02 00:02:35 +0530

    Add from messages and showing incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,26 @@ socket.on('disconnect',function(){
     console.log('disconnected from server');
 });
 
-// socket.emit('createMessage',{
-//     from:'sourav',
-//     text:'sis'
-// });
+socket.emit('createMessage',{
+   from:'Frank',
+   text:'Hi'
+   },function(data){
+    console.log('got it',data);
+});
 
 socket.on('newMessage',function (msg) {
     console.log('New Message',msg);
+    var li=jQuery('<li></li>');
+    li.text(`${msg.from}:${msg.text}`);
+    
+    jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit',function (e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    },function () {
+    });
 });
--- server/server.js
+++ server/server.js
@@ -17,43 +17,15 @@ io.on('connection',(socket) =>{
 
     socket.emit('newMessage',generateMessage('Admin','welcome to group'));
     socket.broadcast.emit('newMessage',generateMessage('Admin','New user found'));
-   // socket.on('createMessage' ,(msg) =>{
-   //      console.log('New Message',msg);
-   //      io.emit('newMessage',generateMessage(msg.from,msg.text));
-   //  });
+    socket.on('createMessage' ,(msg) =>{
+        console.log('New Message',msg);
+        io.emit('newMessage',generateMessage(msg.from,msg.text));
+    });
 
-    //socket.on('createMessage' ,(msg) =>{
-    //    console.log('New Message',msg);
-        // io.emit('newMessage',{
-        //     from:msg.from,
-        //     text:msg.text
-        // };
-
-       // socket.broadcast.emit('newMessage',{
-       //     from:msg.from,
-       //     text:msg.text
-        //});
-   // });
-
-
-        // socket.broadcast.emit('newMessage',()=>{
-    //     from:'sourav', text:'sis'
-    // });
-    //
-    // socket.on('createMessage' ,(msg) =>{
-    //           console.log('New Message',msg);
-    //          io.emit('newMessage',{
-    //              from:msg.from,
-    //              text:msg.sis
-    //          });
-    // });
-
-
-     socket.on('disconnect',(socket) =>{
+    socket.on('disconnect',(socket) =>{
     console.log(`user ${--count} disconnected`);
-  });
+    });
 });
-server.listen(port,() => {
+    server.listen(port,() => {
     console.log(`server is up to on ${port}`);
-});
-//console.log(publicpath);
+    });
